191ad59cdf558f7eeb4cb8d315e378419d37f267
(no author) <(no author)@2b46cb21-ebdf-45b3-a131-b05a2c4c95c3>
2011-10-31 14:36:58 +0000

    crank and reptation moves, no energy func
    git-svn-id: svn://fold3.che.virginia.edu/proteinmontecarlo@1 2b46cb21-ebdf-45b3-a131-b05a2c4c95c3

---
+++ crank.py
@@ -0,0 +1,76 @@
+from numpy import *
+from random import *
+
+def crankshaft(mpos): #moleculeposition, will need bonds later
+    m=randint(1,6) #random molecule, not end ones
+
+    posb=mpos[m][:] # middle molecule, will get crankshafted
+    posa=mpos[m-1][:] #one 'before' it
+    posc=mpos[m+1][:] # one 'after' it
+
+    AB=posb-posa
+    AC=posc-posa
+
+    x=[1,0,0]
+    y=[0,1,0]
+    z=[0,0,1]
+    x1=AC/dot(AC,AC)**.5
+    y1=AB-dot(AB,AC)/dot(AC,AC)*AC
+    y1=y1/dot(y1,y1)**.5
+    z1=cross(x1,y1)
+    untransform=[[dot(x,x1),dot(x,y1),dot(x,z1)],[dot(y,x1),dot(y,y1),dot(y,z1)],[dot(z,x1),dot(z,y1),dot(z,z1)]]    
+    transform=transpose(untransform)
+    AB1=dot(transform,AB.transpose())
+    print(AB1)
+    if randint(1,3) ==1:    
+	#180 deg crank
+        AB1[1]=-AB1[1]
+    elif randint(1,3)==2:
+	#90 deg crank
+	AB1[1],AB1[2]=AB1[2],AB1[1]
+    else:
+        #90 deg other direction
+        AB1[1],AB1[2]=AB1[2],AB1[1]*-1
+    AB=dot(untransform,AB1)
+    mpos[m][:]=posa+AB
+    return mpos
+
+def reptation(mpos):
+    theta=pi*random()
+    phi=2*pi*random()
+    rho=1.53710311951 #unhardcode this later
+    vec=[rho*sin(theta)*cos(phi),rho*sin(theta)*sin(phi),rho*cos(theta)]    
+    n=len(mpos)
+    if random() < .5:    
+	for i in range(n-1):
+             mpos[i][:]=mpos[i+1][:]
+    	mpos[n-1][:]=mpos[n-2][:]+vec
+    else:
+	for i in range(n-1,0,-1):
+	     mpos[i][:]=mpos[i-1][:]
+	mpos[0][:]=mpos[1][:]+vec
+    return mpos
+    
+def writepdb(mpos,text,posline,move):
+    j=0
+    for i in posline:
+        words=text[i].split('  ')
+        words[len(words)-3]=str('%6.3f') % mpos[j][0]
+        words[len(words)-2]=str('%6.3f') % mpos[j][1]
+        words[len(words)-1]=str('%6.3f') % mpos[j][2]+'\r\n'
+        j=j+1
+        text[i]='  '.join(words)
+    f=file(str(move)+'.pdb','w')
+    write=''
+    for k in range(len(text)):
+        write=write+text[k]
+    f.write(write)
+    f.close
+
+def energy(mpos): #need bond for more complex molecules
+    pass
+
+
+
+
+
---
+++ readpdb.py
@@ -0,0 +1,32 @@
+from numpy import *
+from crank import *
+
+openfile=open('polyethConly.pdb','r')
+i=0 # index for coordinate matrix
+k=0 # index for line number
+coord=zeros((8,3))#unhardcode this later
+wtemp=[]
+hetatm=[]
+while 1:
+    line = openfile.readline()
+    if not line:
+        break
+    wtemp.append(line)
+    splitline=line.split('  ')
+    if splitline[0]=='HETATM':
+        hetatm.append(k)
+        n=len(splitline)
+        coord[i][0]=float(splitline[n-3])
+        coord[i][1]=float(splitline[n-2])
+        word=splitline[n-1]
+        coord[i][2]=float(word[:len(word)-2])
+        i=i+1
+    k=k+1
+
+
+for move in range(1):
+	if random() < .5:
+		coord=crankshaft(coord)
+	else:	
+		coord=reptation(coord)
+	writepdb(coord,wtemp,hetatm,move)
