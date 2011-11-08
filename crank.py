2a845b32e1dc8c8af9ee905de9971cff68c39117
(no author) <(no author)@2b46cb21-ebdf-45b3-a131-b05a2c4c95c3>
2011-11-08 15:32:10 +0000

    added torsional energy calculation
    git-svn-id: svn://fold3.che.virginia.edu/proteinmontecarlo@14 2b46cb21-ebdf-45b3-a131-b05a2c4c95c3

--- crank.py
+++ crank.py
@@ -78,7 +78,7 @@ def writepdb(mpos,text,posline,move,filename):
         text[i]=words+coordstr
     f=file('simulate.pdb','w')
     write='MODEL        '+str(move)+'\r\n' #check moves here
-    for k in range(len(text)-1): #dont want 'END'
+    for k in range(len(text)-1): #don't want 'END'
         write=write+text[k]
     f.write(write)
     f.write('ENDMDL\r\n')
@@ -105,7 +105,10 @@ def energy(mpos):
     energy=0.0; #potential energy
     sig=4.6 #angstroms for polyethylene
     e= .42 #kcal/mol for polyethylene
-    ktheta= .82 #kcal/mol 
+    ktheta= .82 #kcal/mol
+    A=5.22
+    B=2.88
+    C=1.95
     index=arange(len(mpos))
     for i in index:
         low=index[index<i-2]
@@ -113,13 +116,22 @@ def energy(mpos):
         vdw=append(low,high) #index of beads excluding 12 and 13 neighbors
         for j in vdw:
             r=((mpos[i][0]-mpos[j][0])**2+(mpos[i][1]-mpos[j][1])**2+(mpos[i][2]-mpos[j][2])**2)**.5
-            energy=energy+4*e*((sig/r)**12-(sig/r)**6)
+            energy=energy+2*e*((sig/r)**12-(sig/r)**6) #divide by two since count each interaction twice
     for i in range(1,len(mpos)-1):
         BA=mpos[:][i-1]-mpos[:][i]
         BC=mpos[:][i+1]-mpos[:][i]
         angle=arccos(dot(BA,BC)/(dot(BA,BA)**.5*dot(BC,BC)**.5)) #in radians
         energy=energy+ktheta/2*(angle-pi)**2
-    return energy/2
+    for i in range(0,len(mpos)-3):
+        AB=mpos[:][i+1]-mpos[:][i]
+        BC=mpos[:][i+2]-mpos[:][i+1]
+        CD=mpos[:][i+3]-mpos[:][i+2]
+        plane1=cross(BC,AB)
+        plane2=crosS(CD,BC)
+        dihedral=arccos((plane1[0]*plane2[0]+plane1[1]*plane2[1]+plane1[2]*plane2[2])/((plane1[0]**2+plane1[1]**2+plane1[2]**2)**.5*(plane2[0]**2+plane2[1]**2+plane2[2]**2)**.5))
+        print(dihedral)
+        energy=energy+A+B*cos(dihedral)+C*cos(3*dihedral)
+    return energy
 
 def enddist(mpos):
     distvec=mpos[:][0]-mpos[:][7] #this is hardcoded
