62f41c16247c79bdecf260ee75eb22e52e40551b
(no author) <(no author)@2b46cb21-ebdf-45b3-a131-b05a2c4c95c3>
2012-05-21 13:27:32 +0000

    temperature rx with pp module, not working
    git-svn-id: svn://fold3.che.virginia.edu/proteinmontecarlo@34 2b46cb21-ebdf-45b3-a131-b05a2c4c95c3

---
+++ simulationobject.py
@@ -0,0 +1,232 @@
+from numpy import *
+from writetopdb import *
+from moveset import *
+from energyfunc import *
+import matplotlib.pyplot as plt
+from sys import stdout
+from random import *
+import profile
+import scipy.misc
+import numpy
+import writetopdb
+import moveset
+import energyfunc
+import matplotlib.pyplot as plt
+
+def energyprint(mpos,rsquare,torsE,angE):
+	LJ=energyfunc.LJenergy_n(rsquare,Simulation.nativeparam_n,Simulation.nonnativeparam,Simulation.nnepsil)
+	energy=sum(angE)+sum(torsE)+LJ
+	print 'Angle energy: ' + str(sum(angE))
+	print 'Torsion energy: '+str(sum(torsE))
+	print 'LJ energy: '+str(LJ)
+	print 'Total energy: '+str(energy)
+	return energy
+
+def energy(mpos,rsquare,torsE,angE):
+	energy=sum(angE)+sum(torsE)+energyfunc.LJenergy_n(rsquare,Simulation.nativeparam_n,Simulation.nonnativeparam,Simulation.nnepsil)
+	return energy
+
+class Simulation:
+	kb=0.0019872041 #kcal/mol/K
+	percentmove=[.33,.66] #% bend,% axis torsion,% crankshaft
+
+	def __init__(self,name,outputdirectory,coord,temp):
+		self.name=name
+		self.out=outputdirectory
+		self.coord=coord
+		self.energyarray=numpy.empty(Simulation.totmoves/Simulation.step+1)
+		self.nc=numpy.empty(Simulation.totmoves/Simulation.step+1) #native contacts
+		self.rmsd_array=numpy.empty(Simulation.totmoves/Simulation.step+1)
+		self.move=0
+		self.T=temp
+		self.r2=energyfunc.getLJr2(self.coord,Simulation.numint,Simulation.numbeads)
+		self.torsE=energyfunc.torsionenergy_nn(self.coord,numpy.zeros(Simulation.numbeads-3),Simulation.torsparam,numpy.arange(Simulation.numbeads-3))
+		self.angE=energyfunc.angleenergy_n(self.coord,numpy.zeros(Simulation.numbeads-2),Simulation.angleparam,numpy.arange(Simulation.numbeads-2))
+		self.u0=energyprint(self.coord,self.r2,self.torsE,self.angE)
+		self.energyarray[0]=self.u0
+		self.rmsd_array[0]=energyfunc.rmsd(Simulation.coord_nat,Simulation.coord_nat)
+		self.nc[0]=energyfunc.nativecontact(self.r2,Simulation.nativeparam_n,Simulation.nsigma2)
+		self.maxtheta=[10*self.T/300.,10*self.T/200.,20*self.T/250.] # bend, axistorsion, crankshaft
+		# constants for move stats
+		self.angmoves=0
+		self.crankmoves=0
+		self.atormoves=0
+		self.acceptedat=0
+		self.accepteda=0
+		self.acceptedc=0
+		self.accepted=0
+		self.rejected=0
+		self.movetype=''
+	
+	def run(self,nummoves,coord,numbeads,step,totmoves,numint,angleparam,torsparam,nativeparam_n,nonnativeparam,nnepsil,nsigma2,transform,coord_nat):
+		Simulation.numbeads=numbeads
+		Simulation.step=step
+		Simulation.totmoves=totmoves
+		Simulation.numint=numint
+		Simulation.angleparam=angleparam
+		Simulation.torsparam=torsparam
+		Simulation.nativeparam_n=nativeparam_n
+		Simulation.nonnativeparam=nonnativeparam
+		Simulation.nnepsil=nnepsil
+		Simulation.nsigma2=nsigma2
+		Simulation.transform=transform
+		Simulation.coord_nat=coord_nat
+		self.coord=coord
+		for i in range(nummoves):
+			self.randmove=random.random()
+			self.randdir=random.random()
+			self.m=random.randint(1,Simulation.numbeads-2) #random bead, not end ones
+			
+			#bend
+			if self.randmove < Simulation.percentmove[0]:
+				self.theta=self.maxtheta[0]/180.*numpy.pi-random.random()*self.maxtheta[0]*numpy.pi/180.*2
+				self.newcoord=moveset.anglebend(self.coord,self.m,self.randdir,self.theta)
+				self.angmoves += 1
+				self.movetype='a'
+				self.change=[]
+				self.angchange=[self.m-1]
+		
+			#axis torsion
+			elif self.randmove < Simulation.percentmove[1]:
+				self.theta=self.maxtheta[1]/180.*numpy.pi-random.random()*numpy.pi*self.maxtheta[1]/180.*2
+				self.newcoord=moveset.axistorsion(self.coord,self.m,self.randdir,self.theta)
+				self.movetype='at'
+				self.atormoves += 1
+				self.angchange=[]
+				if self.randdir<.5:
+					self.change=[self.m-2]
+					if self.m<2:
+						self.change=[]
+				elif self.m==Simulation.numbeads-2:
+					self.change=[]
+				else:
+					self.change=[self.m-1]
+			
+			#crankshaft
+			else:
+				self.theta=self.maxtheta[2]/180.*numpy.pi-random.random()*self.maxtheta[2]*numpy.pi/180.*2
+				self.newcoord=moveset.crankshaft(self.coord,self.m,self.theta)
+				self.crankmoves += 1
+				self.movetype='c'
+				self.change=numpy.arange(self.m-3,self.m+1)
+				self.angchange=[self.m-2,self.m]
+				if self.m==2:
+					self.change=numpy.arange(self.m-2,self.m+1)
+				elif self.m<2:
+					self.change=self.change[self.change>-1]
+					self.angchange=[self.m]
+				elif self.m==Simulation.numbeads-4 or self.m==Simulation.numbeads-3:
+					self.change=self.change[self.change<(Simulation.numbeads-3)]
+				elif self.m==Simulation.numbeads-2:
+					self.change=numpy.arange(self.m-3,self.m-1)
+					self.angchange=[self.m-2]
+			
+			self.r2new=energyfunc.getLJr2(self.newcoord,Simulation.numint,Simulation.numbeads)
+			self.newangE=energyfunc.angleenergy_n(self.newcoord,self.angE,Simulation.angleparam,self.angchange)
+			self.newtorsE=energyfunc.torsionenergy_nn(self.newcoord,self.torsE,Simulation.torsparam,self.change)
+			self.u1=energy(self.newcoord,self.r2new,self.newtorsE,self.newangE)
+			self.move += 1
+			print self.move
+			#stdout.write(str(move)+'\r')
+			#stdout.flush()
+			self.boltz=numpy.exp(-(self.u1-self.u0)/(Simulation.kb*self.T))
+			if self.u1< self.u0 or random.random() < self.boltz:
+				self.accepted += 1
+				if self.movetype=='a':
+					self.accepteda += 1
+				elif self.movetype=='r':
+					self.acceptedr += 1
+				elif self.movetype=='at':
+					self.acceptedat +=1
+					#accepted_angle.append(theta)
+				else: 
+					self.acceptedc += 1
+				#if (pdbfile != ''):
+					#mcoord=moviecoord(newcoord,transform)
+					##writeseqpdb(mcoord,wordtemplate,ATOMlinenum,accepted)
+					#addtopdb(mcoord,positiontemplate,move,pdbfile)
+				self.r2=self.r2new
+				self.coord=self.newcoord
+				self.torsE=self.newtorsE
+				self.angE=self.newangE
+				self.u0=self.u1
+			else:
+				self.rejected += 1
+			if self.move%Simulation.step==0:
+				# energy array
+				self.energyarray[self.move/Simulation.step]=self.u0
+				
+				# native contact array
+				self.nc[self.move/Simulation.step]=energyfunc.nativecontact(self.r2,Simulation.nativeparam_n,Simulation.nsigma2)
+				
+				# rmsd array
+				self.mcoord=writetopdb.moviecoord(self.coord,Simulation.transform)
+				self.rmsd_array[self.move/Simulation.step]=energyfunc.rmsd(Simulation.coord_nat,self.mcoord)
+		return self.u0
+	
+	def output(self,verbose):
+		write='-------- %s Simulation Results --------\r\n' %(self.name) 
+		write += 'total accepted moves: %d \r\n' %(self.accepted)
+		write += 'total rejected moves: %d \r\n' %(self.rejected)
+		write += 'angle bend: %d moves accepted out of %d tries \r\n' %(self.accepteda,self.angmoves)
+		write += 'crankshaft: %d moves accepted out of %d tries \r\n' %(self.acceptedc,self.crankmoves)
+		write += 'torsion: %d moves accepted out of %d tries \r\n' %(self.acceptedat,self.atormoves)
+		if(verbose):
+			print write
+		return write
+	
+	def saveenergy(self,plot):
+		filename=self.out+'/energy'+str(int(self.T))+'.txt'
+		savetxt(filename,self.energyarray)
+		print 'wrote every %d conformation energies to %s' %(Simulation.step,filename)
+		if(plot):
+			plotname=self.out+'energy'+str(int(self.T))+'.png'
+			print 'generating conformational energy plot...'
+			plt.figure(1)
+			plt.plot(range(len(self.energyarray)),self.energyarray)
+			plt.xlabel('move/%d' %(Simulation.step))
+			plt.ylabel('energy (kcal/mol)')
+			plt.title('Go-like model monte carlo simulation at %d K' %(self.T))
+			plt.savefig(plotname)
+			print 'conformational energy plot saved to %s' %(plotname)
+	
+	def savermsd(self,plot):
+		filename=self.out+'/rmsd'+str(int(self.T))+'.txt'
+		savetxt(filename,self.rmsd_array)
+		print 'wrote every %d rmsd values to %s' %(Simulation.step,filename)
+		if(plot):
+			plotname=self.out+'rmsd'+str(int(self.T))+'.png'
+			print 'generating rmsd plot...'
+			plt.figure(2)
+			plt.plot(range(len(self.rmsd_array)),self.rmsd_array)
+			plt.xlabel('move/%d' %(Simulation.step))
+			plt.ylabel('rmsd')
+			plt.title('Go-like model monte carlo simulation at %d K' %(self.T))
+			plt.savefig(plotname)
+			print 'rmsd plot saved to %s' %(plotname)
+		
+	
+	def savenc(self,plot):
+		fraction=self.nc/Simulation.totnc
+		fractionfile=self.out+'/fractionnative'+str(int(self.T))+'.txt'
+		savetxt(fractionfile,self.nc)
+		print 'wrote every %d fractional nativeness values to %s' %(Simulation.step,fractionfile)
+		print 'excluding first '+str(len(fraction)/5)+' out of ' + str(len(fraction))+' fractional nativeness values from average'
+		fractioncut=fraction[len(fraction)/5:-1]
+		print 'the average fractional nativeness is '+str(sum(fractioncut)/len(fractioncut))
+		if(plot):
+			plotname=self.out+'fractionnative'+str(int(self.T))+'.png'
+			print 'generating fractional nativeness plot...'
+			plt.figure(3)
+			plt.plot(range(len(fraction)),fraction)
+			plt.xlabel('move/%d' %(Simulation.step))
+			plt.ylabel('Q, fractional nativeness')
+			plt.title('Go-like model monte carlo simulation at %sd K' %(self.T))
+			plt.savefig(plotname)
+			print 'fractional nativeness plot saved to %s' %(plotname)
+	
+	
+	
+		
+	
+	
---
+++ temprxGO.py
@@ -0,0 +1,280 @@
+
+#========================================================================================================
+# IMPORTS
+#========================================================================================================
+
+from datetime import datetime
+t1=datetime.now()
+from numpy import *
+from writetopdb import *
+from moveset import *
+from energyfunc import *
+from optparse import OptionParser
+import matplotlib.pyplot as plt
+from sys import stdout
+from random import *
+import profile
+import scipy.misc
+from simulationobject import *
+import os
+import pp
+
+parser=OptionParser()
+parser.add_option("-f", "--files", dest="datafile", default="GO_protein.pdb", help="protein .pdb file")
+parser.add_option("-p", "--parameterfile", dest="paramfile", default='GO_protein.param', help="protein .param file")
+parser.add_option("-t", "--temprange", nargs=2, default=[300.0,300.0], type="float", dest="temprange", help="temperature range of replicas")
+parser.add_option("-r", "--replicas", default=1, type="int",dest="replicas", help="number of replicas")
+parser.add_option("-v", "--verbose", action="store_false", default=True, help="more verbosity")
+parser.add_option("-c", "--addconnect", action="store_true", default=False, help="add bonds to .pdb file")
+parser.add_option("-n", "--moves", dest="totmoves", type="int", default='100', help="total number of moves")
+parser.add_option("-s", "--stepsize", dest="step", type="int", default='100', help="number of moves between save operations")
+parser.add_option("-k", "--swapstep", dest="swap", type="int", default='2000', help="number of moves between swap moves")
+parser.add_option("-g", "--histogram", dest="histname", default='', help="name histogram of conformational energies, if desired")
+parser.add_option("-a", "--plot", action="store_true", default=True, help="plots energy, rmsd, fractional nativeness")
+parser.add_option("-b", "--writepdb", dest="pdbfile", default='', help="the output pdb file")
+#parser.add_option("-e", "--percentmove", nargs=2, dest="percentmove", type="float",default=[.33,.66], help="the output pdb file")
+
+(options,args)=parser.parse_args()
+#========================================================================================================
+# CONSTANTS
+#========================================================================================================
+
+
+verbose=options.verbose
+trange=options.temprange#Kelvin
+numreplicas=options.replicas
+totmoves=options.totmoves
+step=options.step
+swap=options.swap
+filename=options.datafile
+paramfile=options.paramfile
+plot=options.plot
+histname=options.histname
+pdbfile=options.pdbfile
+#percentmove=options.percentmove
+addbonds=options.addconnect
+
+kb=0.0019872041 #kcal/mol/K
+nativecutoff2=1.2**2
+
+# read .pdb to get number of beads (numbeads)
+file=open(filename,'r')
+numbeads=0
+while 1:
+	line=file.readline()
+	if not line:
+		break
+	splitline=line.split('  ')
+	if splitline[0]=='ATOM':
+		numbeads += 1
+
+
+if (addbonds):
+	addconnect(pdbfile,numbeads)
+	
+# gets bead coordinates from .pdb file
+file.seek(0)
+coord=empty((numbeads,3)) #3 dimensional
+i=0 # index for coordinate matrix
+k=0 # index for line number
+wordtemplate=[] #template of all words
+positiontemplate=[] #template of position lines
+ATOMlinenum=[] # array of location of ATOM lines
+while 1:
+    line = file.readline()
+    if not line:
+        break
+    wordtemplate.append(line)
+    splitline=line.split('  ')
+    if splitline[0]=='ATOM':
+        positiontemplate.append(line)
+        ATOMlinenum.append(k)
+        coord[i][0]=float(line[31:38])
+        coord[i][1]=float(line[39:46])
+        coord[i][2]=float(line[47:54])
+        i=i+1
+    k=k+1
+file.close()
+
+
+
+#Get native conformation
+#if (rmsdfig != ""):
+
+untransform=getmovietransform(coord)
+transform=transpose(untransform)
+Simulation.transform=transform
+coord_nat=moviecoord(coord,transform)
+Simulation.coord_nat=coord_nat
+
+#Get parameters from .param file
+angleparam=getangleparam(paramfile,numbeads)
+torsparam=gettorsionparam(paramfile,numbeads)
+Simulation.angleparam=angleparam
+Simulation.torsparam=torsparam
+
+#pregenerate list of interactions
+numint=int(scipy.misc.comb(numbeads,2)+1) # number of interactions
+numint= numint - 2*(numbeads-2)-1 # don't count 12 and 13 neighbors
+
+#native LJ parameter getting
+nativeparam_n=getnativefix_n(paramfile,numint,numbeads) # [ones and zeros, native epsilon, native sigma]
+Simulation.nativeparam_n=nativeparam_n
+Simulation.totnc=sum(nativeparam_n[:,0]) #total native contacts
+nsigma2=nativecutoff2*nativeparam_n[:,2]*nativeparam_n[:,2]
+Simulation.nsigma2=nsigma2
+
+#nonnative LJ parameter getting
+nonnativesig=getLJparam_n(paramfile,numbeads,numint) #[nonnative sigmas for every interaction, epsilon (one value)]
+nnepsil=-nonnativesig[-1] # last element
+nonnativesig=delete(nonnativesig,-1) #remove epsilon from sigma array
+nonnatindex=-1*(nativeparam_n[:,0]-1) # array of ones and zeros
+nonnativeparam=column_stack((nonnatindex,nonnativesig)) #[ones and zeros, nonnative sigma]
+Simulation.nonnativeparam=nonnativeparam
+Simulation.nnepsil=nnepsil
+# set additional Simulation class variables
+Simulation.totmoves=totmoves
+Simulation.step=step
+Simulation.numbeads=numbeads
+Simulation.numint=numint
+Simulation.wordtemplate=wordtemplate
+Simulation.positiontemplate=positiontemplate
+Simulation.ATOMlinenum=ATOMlinenum
+
+# Calculate temperature distribution
+if(numreplicas==1):
+	if(trange[0]!=trange[1]):
+		print 'WARNING: using lower temperature bound for one replica simulation'
+	T=[trange[0]]
+elif(numreplicas==2):
+	T=trange
+elif(numreplicas==3):
+	T=empty(numreplicas)
+	T[0]=trange[0]
+	T[-1]=trange[1]
+	T[1]=T[0]+(T[-1]/T[0])**(1./(numreplicas-1))
+else:
+	T=empty(numreplicas)
+	T[0]=trange[0]
+	T[-1]=trange[1]
+	T[1]=T[0]+(T[-1]/T[0])**(1./(numreplicas-1))
+	for i in range(2,numreplicas-1):
+		T[i]=T[0]*(T[1]-T[0])**i
+
+if (verbose):
+	print 'verbosity is %s' %(str(verbose))
+	print 'total number of moves is %d' %(totmoves)
+	print 'autocorrelation step size is %d moves' %(step)
+	print 'there are %d replicas' %(numreplicas)
+	print 'Temperature: '
+	print T
+	print 'the replica exchange interval is %d steps' %(swap)
+	print 'There are %d residues in %s' %(numbeads,filename)
+
+
+def tryswap(Simulations,lastenergy):
+	if(numreplicas==1):
+		pass
+	else:
+		i=randint(0,numreplicas-2) #tests swap between replica i and i+1
+		print i
+		P=exp((lastenergy[i+1]-lastenergy[i])*(1/(kb*T[i])-1/(kb*T[i+1])))
+		if(random()<P):
+			Simulations[i].coord, Simulations[i+1].coord = Simulations[i+1].coord, Simulations[i].coord
+
+def pprun(Simulations,moves):
+	jobs=[job_server.submit(Simulations[i].run,(moves,Simulations[i].coord,numbeads,step,totmoves,numint,angleparam,torsparam,nativeparam_n,nonnativeparam,nnepsil,nsigma2,transform,coord_nat),(energy,energyprint),('random','numpy','energyfunc','moveset','writetopdb')) for i in range(numreplicas)]
+	lastenergy=[job() for job in jobs]
+	return lastenergy
+
+def energyprint(mpos,rsquare,torsE,angE):
+	LJ=energyfunc.LJenergy_n(rsquare,Simulation.nativeparam_n,Simulation.nonnativeparam,Simulation.nnepsil)
+	energy=sum(angE)+sum(torsE)+LJ
+	print 'Angle energy: ' + str(sum(angE))
+	print 'Torsion energy: '+str(sum(torsE))
+	print 'LJ energy: '+str(LJ)
+	print 'Total energy: '+str(energy)
+	return energy
+
+def energy(mpos,rsquare,torsE,angE):
+	energy=sum(angE)+sum(torsE)+energyfunc.LJenergy_n(rsquare,Simulation.nativeparam_n,Simulation.nonnativeparam,Simulation.nnepsil)
+	return energy
+
+
+#========================================================================================================
+# SIMULATE
+#========================================================================================================
+
+# instantiate replicas
+replicas=[]
+for i in range(len(T)):
+	direc='./replicaexchange/replica'+str(i)
+	if not os.path.exists(direc):
+		os.mkdir(direc)
+	name='replica'+str(i)
+	replicas.append(Simulation(name,direc,coord,T[i]))
+
+#result=replicas[0].run(1000,replicas[0].coord)
+job_server=pp.Server()
+#print job_server
+#jobs=job_server.submit(replicas[0].run,(1000,replicas[0].coord,numbeads,step,totmoves,numint,angleparam,torsparam,nativeparam_n,nonnativeparam,nnepsil,nsigma2,transform,coord_nat),(energy,energyprint),modules=('random','numpy','energyfunc','moveset','writetopdb'))
+#result=jobs()
+#print result
+
+job_server=pp.Server()
+move=0
+print move
+
+for i in range(totmoves/swap):
+	currentenergy=pprun(replicas,swap)
+	tryswap(replicas,currentenergy)
+	move += totmoves/swap
+	print move
+
+e=pprun(replicas,totmoves%swap)
+print e
+move += totmoves%swap
+print move
+
+output=''
+for replica in replicas:
+	output+=replica.output(verbose)
+	replica.saveenergy(plot)
+	replica.savermsd(plot)
+	replica.savenc(plot)
+print output
+f=open('./replicaexchange/output.txt','w')
+f.write(output)
+f.close	
+	
+	
+t2=datetime.now()
+
+#if (pdbfile != ''):
+	#f=open(pdbfile,'a')
+	#f.write('END\r\n')
+	#f.close
+	#print 'wrote trajectory to %s' %(pdbfile)
+
+
+##========================================================================================================
+## OUTPUT
+##========================================================================================================
+
+#fraction=nc/totnc
+#print 'excluding first '+str(len(fraction)/5)+' native contact values from average'
+#fraction=fraction[len(fraction)/20:-1]
+#fractionfile='fractionnative'+str(int(T))+'.txt'
+#savetxt(fractionfile,fraction)
+#print 'wrote ever %d conformation fraction native contacts to %s' %(step,fractionfile)
+#print sum(fraction)/len(fraction)
+
+
+print('Simulation time: '+str(t2-t1))
+
+#x=range(len(rmsd_array))
+#plt.plot(x,rmsd_array)
+#plt.ylabel('RMSD')
+#plt.xlabel('move/100')
+#plt.title('RMSD from native conformation at %f Kelvin taken every %d moves' %(T,step))
+#plt.savefig(rmsdname)
